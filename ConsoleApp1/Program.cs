7da4a2e2986f7824d51be634c4d40f701a8d228a
Nata <nazaharova29@gmail.com>
2024-09-01 17:16:00 +0300

    Юнит 4.1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,16 +4,31 @@ class MainClass
 {
     public static void Main(string[] args)
     {
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        
-        Console.Write("Enter your name: ");
-        var name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        var age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-        Console.Write("Enter your birthdate: ");
-        var birthdate = Console.ReadLine();
-        Console.WriteLine($"Your birthdate is {birthdate} ");
+        var color = Console.ReadLine();
 
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
     }
 }
